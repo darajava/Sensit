4a03b654ae4c3c05c287d58add9484afd60375c1
darajava <darajavaherian@gmail.com>
2017-12-19 18:04:30 +1100

    Tighen up last online

--- src/components/RoomHeader/RoomHeader.js
+++ src/components/RoomHeader/RoomHeader.js
@@ -10,25 +10,40 @@ let hash = require('object-hash');
 
 const RoomHeader = (props) => {
 
+  let iscurrentDate = moment(props.user.lastOnline).isSame(new Date(), "day");
+  let isOnline = moment().diff(moment(props.user.lastOnline), 'minutes') < 1
+
+  let dateString; 
+
+  if (isOnline) {
+    dateString = 'online';
+  } else if (iscurrentDate) {
+    dateString = "last seen today at " + moment(props.user.lastOnline).format('h:mma');
+  } else {
+    console.log(props.user.lastOnline)
+    dateString = "last seen " + moment(props.user.lastOnline).format('DD/MM/YYYY h:mma');
+  }
+
   return (
       <div styleName='chat-item'>
-        <Link to={{ pathname: "/home", }} >
+        <div styleName='back-button'>
+          <Link to={{ pathname: "/home", }} >
             <span styleName='glyph'>
               <Glyphicon glyph="menu-left" />
             </span>
 
             <img styleName='profile-image' src={props.user.image ? props.user.image : 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' } />
-        </Link>
+          </Link>
+        </div>
         
-        <span styleName="username">
-          {props.user.username}
-        </span>
-
-
-        <span styleName="username">
-          { moment(props.user.lastOnline).format('h:mm:ss a') }
-          { typeof props.user.lastOnline }
-        </span>
+        <div styleName="user-info">
+          <div styleName="username">
+            {props.user.username}
+          </div>
+          <div styleName="last-online">
+            { dateString }
+          </div>
+        </div>
 
         <span styleName="options">
           <Glyphicon glyph="option-vertical" />
