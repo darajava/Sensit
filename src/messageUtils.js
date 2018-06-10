e1fb10776a7fe64bd0800c6748acf721f9d8c9fc
Dara Javaherian <darajavaherian@gmail.com>
2018-06-10 20:20:37 +0200

    add file

---
+++ src/messageUtils.js
@@ -0,0 +1,43 @@
+export function isDelivered(message) {
+  message.deliveredTo.push(localStorage.getItem('id'));
+
+  for (let i = 0; i < message.forUsers.length; i++) {
+    if (!message.deliveredTo.includes(message.forUsers[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function isLastSeen(message, props) {    
+  for (let i = props.messages.length - 1; i >= 0; i--) {
+    if (isSeen(props.messages[i])) {
+      if (props.messages[i]._id !== message._id) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+export function isSeen(message) {
+  if (!message.seenBy) message.seenBy = [];
+
+  if (!message.seenBy.includes(message.sentBy)) {
+    message.seenBy.push(message.sentBy);    
+  }
+  
+  // console.log('seenby', message.seenBy);
+  // console.log(localStorage.getItem('id'));
+  for (let i = 0; i < message.forUsers.length; i++) {
+    if (!message.seenBy.includes(message.forUsers[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
