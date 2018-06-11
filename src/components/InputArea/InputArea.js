7013d849e4638d777cf0ae82a1a614ad8ea56a68
Dara Javaherian <darajavaherian@gmail.com>
2018-06-12 01:38:46 +0200

    add youtube video embed expansion

--- src/components/InputArea/InputArea.js
+++ src/components/InputArea/InputArea.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 import CSSModules from 'react-css-modules';
 import styles from './styles.css';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import {returnRich} from '../Message/Message';
+import ReactDOMServer from 'react-dom/server';
+
+function setEndOfContenteditable(contentEditableElement)
+{
+    var range,selection;
+    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection();//get the selection object (allows you to change selection)
+        selection.removeAllRanges();//remove any selections already made
+        selection.addRange(range);//make the range you have just created the visible selection
+    }
+    else if(document.selection)//IE 8 and lower
+    { 
+        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+        range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        range.select();//Select the range (make it the visible selection
+    }
+}
 
 const InputArea = (props) => {
 
@@ -15,6 +38,11 @@ const InputArea = (props) => {
     } else {
       if (e.key.length == 1) {
         props.typing(true);
+        let elem = document.getElementById('message-box');
+        // elem.innerHTML = ReactDOMServer.renderToStaticMarkup(
+        //   returnRich(elem.innerHTML)
+        // );
+        // setEndOfContenteditable(elem);
       }
     }
   };
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -5,7 +5,7 @@ import CSSModules from 'react-css-modules';
 import styles from './styles.css';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 
-let returnRich = (text) => {
+export function returnRich (text) {
   var bold = /\*(\S([^*.]*?\S)?)\*/gm;
   var html = text.replace(bold, '<strong>$1</strong>');  
   
@@ -21,11 +21,36 @@ let returnRich = (text) => {
 
 }
 
+
+function strip(html){
+   var doc = new DOMParser().parseFromString(html, 'text/html');
+   return doc.body.textContent || "";
+}
+
+function hasYoutube(url) {
+  var regExp = /.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[2].length == 11) {
+    return match[2];
+  } else {
+    return false;
+  }
+}
+
 export function decode(html) {
   let text = document.createElement('textarea');
   text.innerHTML = html;
 
-  let value = text.value;
+  let value = text.value.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+    return '&#'+i.charCodeAt(0)+';';
+  });
+
+  let yt = hasYoutube(value);
+
+  if (yt.length) {
+    return returnRich('<iframe width="420" height="315" src="https://www.youtube.com/embed/' + yt + '"></iframe>');
+  }
+
 
 
 
